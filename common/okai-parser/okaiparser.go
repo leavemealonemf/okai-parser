de2e7282d2765ff0b88749f525c68074dd5f277c
leavemealonemf <strangemisterio78@gmail.com>
2025-03-22 12:59:51 +0300

    upd

--- common/okai-parser/okaiparser.go
+++ common/okai-parser/okaiparser.go
@@ -2,7 +2,6 @@ package okaiparser
 
 import (
 	"fmt"
-	"math/rand"
 	okaiparsetools "okai/common/okai-parse-tools"
 	"okai/common/utils"
 	"strings"
@@ -142,7 +141,7 @@ func CommandBuilder(cmd map[string]string, tc string) string {
 
 	switch cmd["head"] {
 	case "GTRTO":
-		return fmt.Sprintf("AT+GTRTO=zk200,%s,,%x,,,,,,%s$", cmd["subcommand"], rand.Int(), tc)
+		return fmt.Sprintf("AT+GTRTO=zk200,%s,,%x,,,,,,%s$", cmd["subcommand"], 0, tc)
 	case "GTECC":
 		return fmt.Sprintf("AT+GTECC=zk200,,%s,,1,,,,,,%s$", cmd["subcommand"], tc)
 	default:
